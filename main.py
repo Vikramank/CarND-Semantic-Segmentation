6320675db81d36b08cea265afd95c92fea50d307
Vikramank <vikramank@hotmail.com>
2017-10-29 22:33:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFl
 print('TensorFlow Version: {}'.format(tf.__version__))
 
 # Check for a GPU
+
 if not tf.test.gpu_device_name():
     warnings.warn('No GPU found. Please use a GPU to train your neural network.')
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+   
 
 
 def load_vgg(sess, vgg_path):
@@ -24,16 +26,27 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+
+
     vgg_tag = 'vgg16'
+    #loading the saved model
+    tf.saved_model.loader.load(sess,[vgg_tag], vgg_path)
+    graph=tf.get_default_graph()
+    #input tensor
     vgg_input_tensor_name = 'image_input:0'
+    image_input=graph.get_tensor_by_name(vgg_input_tensor_name)
     vgg_keep_prob_tensor_name = 'keep_prob:0'
+    keep_prob=graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
     vgg_layer3_out_tensor_name = 'layer3_out:0'
+    layer3_out=graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
     vgg_layer4_out_tensor_name = 'layer4_out:0'
+    layer4_out=graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+    layer7_out=graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
+    print("Load VGG success")
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -45,9 +58,43 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param vgg_layer3_out: TF Tensor for VGG Layer 7 output
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
-    """
-    # TODO: Implement function
-    return None
+    
+     """
+    
+    ####### FCN 32
+    
+    #### From dense layer to 1 by 1 convolution of the last layer
+    layer_7_1_by_1=tf.layers.conv2d(vgg_layer7_out, num_classes,1,padding='same',
+                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    ## Upsample the data
+    layer_7_upsample=tf.layers.conv2d_transpose(layer_7_1_by_1,num_classes,4,2,padding='same', 
+                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    layer_4_1_by_1=tf.layers.conv2d(vgg_layer4_out,num_classes,1,padding='same',activation=tf.nn.elu,
+                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    fcn32=tf.add(layer_4_1_by_1,layer_7_upsample)
+    
+    ####### FCN 16
+    
+    #### From dense layer to 1 by 1 convolution of the last layer
+    layer_3_1_by_1=tf.layers.conv2d(vgg_layer3_out, num_classes,1,1,padding='same',
+                                    kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    ## Upsample the data
+    layer_fcn32_upsample=tf.layers.conv2d_transpose(fcn32,num_classes,4,2,padding='same',
+                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    fcn16=tf.add(layer_3_1_by_1,layer_fcn32_upsample)
+    
+    ###fcn8
+    fcn8=tf.layers.conv2d_transpose(fcn16,num_classes,16,8,padding='same',
+                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    
+    
+    return fcn8
 tests.test_layers(layers)
 
 
@@ -61,7 +108,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    
+    
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    
+    #optimiser -
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)    
+    
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -80,6 +135,13 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
+    print("Commencing training")
+    sess.run(tf.global_variables_initializer())
+    for i in range(epochs):
+          print("epoch")
+          for image, label in get_batches_fn(batch_size):
+              sess.run(train_op, feed_dict={input_image:image,correct_label:label,keep_prob:0.8,learning_rate: 0.0005})
+                             
     # TODO: Implement function
     pass
 tests.test_train_nn(train_nn)
@@ -101,9 +163,26 @@ def run():
 
     with tf.Session() as sess:
         # Path to vgg model
+        
+        epochs=2
+        batch_size=8
+        # Creating a Deep neural network
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
         get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+
+        image_input, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out=load_vgg(sess, vgg_path)
+        
+        nn_last_layer=layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        
+        learning_rate=tf.placeholder(tf.float64)
+        correct_label=tf.placeholder(tf.int32,shape=[None,None,None,num_classes])
+
+        logits, train_op, cross_entropy_loss=optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+        # training
+        
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, image_input, correct_label, keep_prob, learning_rate)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, image_input)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
